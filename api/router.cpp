d3b376a461aab136f9148143f6d45ab87045e9e1
Koumiss2 <rrdnd@inbox.ru>
2026-05-14 21:18:35 +0500

    feat: hadlers for methods

--- api/router.cpp
+++ api/router.cpp
@@ -1,16 +1,34 @@
 #include "router.hpp"
-#include "IController.hpp"
-#include <cstring>
-
-using HandlerFunc = std::function<void(const Request& req, Response& res)>;
-
-struct Route {
-    HttpMethod method;
-    std::string_view uri;
-    HandlerFunc handler;
-};
 
 Router& Router::instance() {
     static Router inst;
     return inst;
+}
+
+void Router::get(std::string_view uri, HandlerFunc handler) {
+    _routes.push_back({HttpMethod::GET, uri, handler});
+}
+
+void Router::post(std::string_view uri, HandlerFunc handler) {
+    _routes.push_back({HttpMethod::POST, uri, handler});
+}
+
+void Router::put(std::string_view uri, HandlerFunc handler) {
+    _routes.push_back({HttpMethod::PUT, uri, handler});
+}
+
+void Router::del(std::string_view uri, HandlerFunc handler) {
+    _routes.push_back({HttpMethod::DELETE, uri, handler});
+}
+
+void Router::dispatch(const Request& req, Response& res) const {
+    for (const auto& route : _routes) {
+        if (route.method == req.method && route.uri == req.uri) {
+            route.handler(req, res);
+            return;
+        }
+    }
+
+    res.status = HttpStatus::NOT_FOUND;
+    res.body = "{\"error\":\"route not found\"}";
 }
--- api/router.hpp
+++ api/router.hpp
@@ -1,13 +1,34 @@
 #pragma once
+
 #include "http_types.hpp"
 #include <cstdint>
+#include <vector>
+#include <string_view>
+#include <functional>
 
-class IController;
+using HandlerFunc = std::function<void(const Request& req, Response& res)>;
+
+struct Route {
+    HttpMethod method;
+    std::string_view uri;
+    HandlerFunc handler;
+};
 
 class Router {
 public:
     static Router& instance();
 
+    Router(const Router&) = delete;
+    Router& operator=(const Router&) = delete;
+
+    void get(std::string_view uri, HandlerFunc handler);
+    void post(std::string_view uri, HandlerFunc handler);
+    void put(std::string_view uri, HandlerFunc handler);
+    void del(std::string_view uri, HandlerFunc handler);
+
+    void dispatch(const Request& req, Response& res) const;
+
 private:
     Router() = default;
+    std::vector<Route> _routes;
 };
